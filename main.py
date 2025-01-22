0434326904f8a34f4fc055bc3c1031cb6be5160c
DenchikGol <nogpoctok@yandex.ru>
2025-01-22 12:50:57 +0500

    prepare main for yandex cloud

--- main.py
+++ main.py
@@ -1,24 +1,28 @@
-import asyncio
-import logging
+import json
 
-from aiogram import Bot, Dispatcher
+# import logging
+from aiogram import Bot, Dispatcher, types
 
 from callbacks import callback_router
-from database import create_tables
 from env import API_TOKEN
 from handlers import handlers_router
 
-logger = logging.getLogger(__name__)
+# logger = logging.getLogger(__name__)
+# logging.basicConfig(level=logging.INFO)
+bot = Bot(token=API_TOKEN)
+dp = Dispatcher()
+dp.include_routers(handlers_router, callback_router)
 
 
-async def main():
-    bot = Bot(token=API_TOKEN)
-    dp = Dispatcher()
-    dp.include_routers(handlers_router, callback_router)
-    logging.basicConfig(level=logging.INFO, filename="t_bot.log")
-    await create_tables()
-    await dp.start_polling(bot)
+async def process_event(event):
+    update = types.Update.model_validate(
+        json.loads(event["body"]), context={"bot": bot}
+    )
+    await dp.feed_update(bot, update)
 
 
-if __name__ == "__main__":
-    asyncio.run(main())
+async def yandex_main(event, context):
+    if event["httpMethod"] == "POST":
+        await process_event(event)
+        return {"statusCode": 200, "body": "ok"}
+    return {"statusCode": 405}
